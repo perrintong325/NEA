f87e2f73e1b486c23cf8f2db6d093a9b3a821d70
perrintong325 <perrintong@icloud.com>
2020-11-03 15:33:29 +0000

    Initial Commit

---
+++ NEA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NEA
+//
+//  Created by Perrin Tong on 11/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NEA/NEAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEAApp.swift
+//  NEA
+//
+//  Created by Perrin Tong on 11/3/20.
+//
+
+import SwiftUI
+
+@main
+struct NEAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
